45ace392ef25e1e8a33a4e6305016d0ba276943b
Durzdor <maxiperez024@hotmail.com>
2021-08-21 22:36:13 -0300

    MoveAnimation
    + Idle
    + Walk
    + Sprint

--- DSVJI-2C2021UADE/Assets/Scripts/Character/Character.cs
+++ DSVJI-2C2021UADE/Assets/Scripts/Character/Character.cs
@@ -6,11 +6,22 @@ public class Character : MonoBehaviour
 {
     private Health healthController;
     private Armor armorController;
+    private Animator animator;
+    private CharacterController characterController;
+    private ThirdPersonController thirdPersonController;
+
+    public Animator Animator => animator;
+    public CharacterController Controller => characterController;
+    public ThirdPersonController ThirdPersonController => thirdPersonController;
 
     private void Awake()
     {
         healthController = GetComponent<Health>();
         armorController = GetComponent<Armor>();
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        thirdPersonController = GetComponent<ThirdPersonController>();
+        
         healthController.OnDamaged += OnHealthDamageTaken;
         armorController.OnDamaged += OnArmorDamageTaken;
     }
--- DSVJI-2C2021UADE/Assets/Scripts/Character/CharacterAnimation.cs
+++ DSVJI-2C2021UADE/Assets/Scripts/Character/CharacterAnimation.cs
@@ -1,7 +1,58 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Animator), typeof(Character))]
+[RequireComponent(typeof(Character))]
 public class CharacterAnimation : MonoBehaviour
 {
+    private Character character;
     
+    private static readonly int JumpTrigger = Animator.StringToHash("JumpTrigger");
+    private static readonly int GroundFloat = Animator.StringToHash("GroundFloat");
+    private static readonly int AirFloat = Animator.StringToHash("AirFloat");
+    private static readonly int IdleBool = Animator.StringToHash("Idling");
+    
+    private void Start()
+    {
+        character = GetComponent<Character>();
+        character.ThirdPersonController.OnJump += JumpHandler;
+    }
+    
+    // Animations to code 
+    // Idle, CrouchSneakLeft, CrouchSneakRight, CrouchToStand, FallingIdle, FallToLand
+    // Idle, Jump, Run, StandToCrouch, StumbleBackwards, StumbleToStand, SwitchCharacter
+    // Walk
+
+    private void Update()
+    {
+        if (character.Controller.isGrounded)
+        {
+            character.Animator.SetFloat(AirFloat, 0);
+            if (character.Controller.velocity.magnitude > 0.001f)
+            {
+                character.Animator.SetBool(IdleBool, false);
+                character.Animator.SetFloat(GroundFloat, character.Controller.velocity.magnitude);
+                character.Animator.ResetTrigger(JumpTrigger);
+            }
+
+            if (character.Controller.velocity.magnitude < 0.001f)
+            {
+                character.Animator.SetFloat(GroundFloat, 0f);
+            }
+        }
+        if (!character.Controller.isGrounded)
+        {
+            NotGrounded();
+            character.Animator.SetFloat(AirFloat, Mathf.Abs(character.Controller.velocity.y) + 0.1f);
+        }
+    }
+    
+    private void JumpHandler()
+    {
+        character.Animator.SetTrigger(JumpTrigger);
+    }
+    
+    private void NotGrounded()
+    {
+        character.Animator.SetFloat(GroundFloat, 0);
+        character.Animator.SetBool(IdleBool, false);
+    }
 }
