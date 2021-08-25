ac8e6baf3a3d965e4b4683e9bbe1cbf82f1a509d
DESKTOP-D30550E\jalil <jalilputer@gmail.com>
2021-08-24 22:26:02 -0300

    Se agrega funcion para sacar y poner el arma

--- DSVJI-2C2021UADE/Assets/Scripts/Ruth/RuthWeaponController.cs
+++ DSVJI-2C2021UADE/Assets/Scripts/Ruth/RuthWeaponController.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class RuthWeaponController : MonoBehaviour
 {
-    
-    [SerializeField] private GameObject _rightHand;
-    [SerializeField] private GameObject _leftHand;
-    
-    [SerializeField] private GameObject _primaryRestPosition;
-    [SerializeField] private GameObject _secondaryRestPosition;
+    [SerializeField] private GameObject _equippedPrimary;
+    [SerializeField] private GameObject _equippedSecondary;
 
-    [SerializeField] private GameObject _primaryWeapon;
-    [SerializeField] private GameObject _secondaryWeapon;
+    [SerializeField] private GameObject _unequippedPrimary;
+    [SerializeField] private GameObject _unequippedSecondary;
     
     private bool _drawn;
 
     public void DrawSaveWeapon()
     {
-        _primaryWeapon.transform.position = _drawn ? _primaryRestPosition.transform.position : _rightHand.transform.position;
-        _primaryWeapon.transform.rotation = _drawn ? _primaryRestPosition.transform.rotation : _rightHand.transform.rotation;
-        _secondaryWeapon.transform.position = _drawn ?  _secondaryRestPosition.transform.position : _leftHand.transform.position;
-        _secondaryWeapon.transform.rotation = _drawn ?  _secondaryRestPosition.transform.rotation : _leftHand.transform.rotation;
-
         _drawn = !_drawn;
+        
+        _equippedPrimary.SetActive(_drawn);
+        _equippedSecondary.SetActive(_drawn);
+
+        _unequippedPrimary.SetActive(!_drawn);
+        _unequippedSecondary.SetActive(!_drawn);
+        
+
     }
 }
