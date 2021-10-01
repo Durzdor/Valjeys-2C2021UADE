51c58c3ee67f87e4e52ee91fe1a9d68a76d9788f
DaxterGaston <daxtergaston@hotmail.com>
2021-10-01 15:45:36 -0300

    Merge.

---
+++ DSVJI-2C2021UADE/Assets/Scripts/Utils/OutOfBounds.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var player = GetComponent<Character>();
+            var teleportPos = player.CheckpointRespawn;
+            player.Teleport(teleportPos);
+        }
+    }
+}
