26b0052d547975c3921bad914903a34a76a8959b
DaxterGaston <daxtergaston@hotmail.com>
2021-11-08 23:23:48 -0300

    Se agrega script PORQUE ALGUIEN QUE NO VAMOS A DECIR EL NOMBRE uso un script rancio que encontro en internet en vez de RigidBody para el movimiento.

---
+++ DSVJI-2C2021UADE/Assets/Scripts/Enemies/Generics/PhysicsFix.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// MAXI, TE ODIOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+/// </summary>
+public class PhysicsFix : MonoBehaviour
+{
+    private Vector3 _rot;
+    private Vector3 _fixedVelocity;
+    Rigidbody _rb;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rot = new Vector3(0, 0, 0);
+        _fixedVelocity = new Vector3();
+    }
+    private void LateUpdate()
+    {
+        _rot.y = transform.localEulerAngles.y;
+        transform.localEulerAngles = _rot;
+        _fixedVelocity = _rb.velocity;
+        _fixedVelocity.y = Mathf.Clamp(_fixedVelocity.y, float.MinValue, 1f);
+        _rb.velocity = _fixedVelocity;
+    }
+}
